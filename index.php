1bceab24c810aeb099b10a490b44bb6353675ab7
MikentGuo <gsg7688@gmail.com>
2021-10-22 15:05:16 -0400

    fix error and warning in index.php

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-session_start();
 require_once 'vendor/autoload.php';
 require_once 'init.php';
 require_once 'utils.php';
@@ -7,7 +6,6 @@ require_once 'utils.php';
 // Define app routes below
 require_once 'user.php';
 require_once 'admin.php';
-require_once 'auctions.php';
 
 // Run app - must be the last operation
 // if you forget it all you'll see is a blank page
