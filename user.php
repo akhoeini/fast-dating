e9dabaa46e53a753cbbe38be943e528faec44938
Kevin <kevin.paul@johnabbottcollege.net>
2021-10-26 12:58:57 -0400

    db query

--- user.php
+++ user.php
@@ -11,8 +11,8 @@ require_once 'init.php';
 // $app->get('/logout', function .....);
 
 $app->get('/profile/{uId:[0-9]+}', function($request, $response, $args) {
-    $userId = $args['uId'];
 
+    $userInfo = DB::queryFirstRow("SELECT * FROM users WHERE id=%i", $args['uId']);
 
     // Display 'edit profile' button if user is viewing own profile
     if ($userId = $_SESSION['userId']) {
