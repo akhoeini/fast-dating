fb8dc40a05c04708968ca4e7d99212c4dcc84a15
Amin <mohammadamin.khoeini@gmail.com>
2021-11-01 12:58:04 -0400

    add signup to our root

--- user.php
+++ user.php
@@ -7,9 +7,13 @@ require_once 'init.php';
 // $app->get('/register', function .....);
 
 // Define app routes
-$app->get('/login', function ($request, $response, $args) {
-   return $this->view->render($response, 'login.html.twig');
+$app->get('/signup', function ($request, $response, $args) {
+   return $this->view->render($response, 'signup.html.twig');
 });
+
+$app->get('/login', function ($request, $response, $args) {
+    return $this->view->render($response, 'login.html.twig');
+ });
 
 $app->get('/profile/{uId:[0-9]+}', function($request, $response, $args) {
 
