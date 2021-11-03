d70e57515fc347e8bcfb23437fd869557188bde4
Kevin <kevin.paul@johnabbottcollege.net>
2021-11-03 00:44:11 -0400

    Profile view

--- user.php
+++ user.php
@@ -22,10 +22,12 @@ $app->get('/login', function ($request, $response, $args) {
 $app->get('/profile/{uId:[0-9]+}', function($request, $response, $args) {
 
     $userInfo = DB::queryFirstRow("SELECT * FROM users WHERE id=%i", $args['uId']);
-
-    // Display 'edit profile' button if user is viewing own profile
-    if ($userId = $_SESSION['userId']) {
-        return $this->view->render($response, 'profile.html.twig', ['ownProfile' => true, 'uId' => $userId]);
+    if (!$userInfo) {
+        throw new \Slim\Exception\NotFoundException($request, $response);
+    } else {
+        // Will display 'edit profile' button if user is viewing own profile
+        $isOwnProfile = ($args['uId'] == $_SESSION['userId']);
+        return $this->view->render($response, 'profile.html.twig', ['u' => $userInfo, 'ownProfile' => $isOwnProfile] );
     }
 
 });
