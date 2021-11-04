4228bcb53dc3b8d9cfa142bb0b2b52612764e5f3
Kevin <kevin.paul@johnabbottcollege.net>
2021-11-04 00:55:51 -0400

    profile interests

--- user.php
+++ user.php
@@ -23,12 +23,13 @@ $app->get('/profile/{uId:[0-9]+}', function($request, $response, $args) {
 
     $userInfo = DB::queryFirstRow("SELECT * FROM users WHERE id=%i", $args['uId']);
     $userPhotos = DB::query("SELECT photo.name, photo.description, photo.url FROM albums, photo WHERE albums.ownerId=%i AND albums.id=photo.albumId", $args['uId']);
+    $userInterests = DB::queryFirstColumn("SELECT interests.name FROM userInterest, interests WHERE userInterest.userId=%i AND userInterest.interestId=interests.id", $args['uId']);
     if (!$userInfo) {
         throw new \Slim\Exception\NotFoundException($request, $response);
     } else {
         // Will display 'edit profile' button if user is viewing own profile
         $isOwnProfile = ($args['uId'] == $_SESSION['userId']);
-        return $this->view->render($response, 'profile.html.twig', ['u' => $userInfo, 'pics' => $userPhotos, 'ownProfile' => $isOwnProfile] );
+        return $this->view->render($response, 'profile.html.twig', ['u' => $userInfo, 'pics' => $userPhotos, 'interests' => $userInterests, 'ownProfile' => $isOwnProfile] );
     }
 });
 
