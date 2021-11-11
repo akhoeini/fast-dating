d06da887d3dd5cda0a3524cf7a03a4f97238bf8e
Amin <mohammadamin.khoeini@gmail.com>
2021-11-11 13:41:22 -0500

    update database with sever expoert file

--- db-fast-date.sql
+++ db-fast-date.sql
@@ -1,132 +1,387 @@
-ALTER TABLE `room` DROP FOREIGN KEY `fk_room`;
-ALTER TABLE `participants` DROP FOREIGN KEY `fk_participants`;
-ALTER TABLE `participants` DROP FOREIGN KEY `fk_participants_1`;
-ALTER TABLE `message` DROP FOREIGN KEY `fk_message`;
-ALTER TABLE `message` DROP FOREIGN KEY `fk_message_1`;
-ALTER TABLE `photo` DROP FOREIGN KEY `fk_photo`;
-ALTER TABLE `albums` DROP FOREIGN KEY `fk_albums_1`;
-ALTER TABLE `users` DROP FOREIGN KEY `fk_users_1`;
-ALTER TABLE `matches` DROP FOREIGN KEY `fk_matches`;
-ALTER TABLE `matches` DROP FOREIGN KEY `fk_matches_1`;
-ALTER TABLE `userInterest` DROP FOREIGN KEY `fk_userInterest`;
-ALTER TABLE `userInterest` DROP FOREIGN KEY `fk_userInterest_1`;
-ALTER TABLE `userLookingForId` DROP FOREIGN KEY `fk_userLookingForId`;
-ALTER TABLE `userLookingForId` DROP FOREIGN KEY `fk_userLookingForId_1`;
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Nov 11, 2021 at 07:40 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.11
 
-DROP INDEX `` ON `users`;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-DROP TABLE `users`;
-DROP TABLE `albums`;
-DROP TABLE `photo`;
-DROP TABLE `message`;
-DROP TABLE `room`;
-DROP TABLE `participants`;
-DROP TABLE `roomType`;
-DROP TABLE `genders`;
-DROP TABLE `matches`;
-DROP TABLE `interests`;
-DROP TABLE `userInterest`;
-DROP TABLE `userLookingForId`;
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dating`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `albums`
+--
+
+CREATE TABLE `albums` (
+  `id` int(11) NOT NULL,
+  `name` enum('profile','general') DEFAULT 'general',
+  `description` varchar(255) DEFAULT NULL,
+  `category` varchar(255) DEFAULT NULL,
+  `ownerId` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `albums`
+--
+
+INSERT INTO `albums` (`id`, `name`, `description`, `category`, `ownerId`) VALUES
+(1, '', 'testimgtestimgtestimg', 'general', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `interests`
+--
+
+CREATE TABLE `interests` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `description` varchar(500) DEFAULT NULL,
+  `type` enum('other','indoor','outdoor','tech','animals') NOT NULL DEFAULT 'other'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `interests`
+--
+
+INSERT INTO `interests` (`id`, `name`, `description`, `type`) VALUES
+(1, 'Technology', NULL, 'tech'),
+(2, 'Cooking', NULL, 'other');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `matches`
+--
+
+CREATE TABLE `matches` (
+  `uId1` int(11) NOT NULL,
+  `uId2` int(11) NOT NULL,
+  `Id` int(11) NOT NULL,
+  `isActive` bit(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `message`
+--
+
+CREATE TABLE `message` (
+  `id` int(11) NOT NULL,
+  `roomId` int(11) DEFAULT NULL,
+  `userId` int(11) DEFAULT NULL,
+  `message` varchar(1000) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `participants`
+--
+
+CREATE TABLE `participants` (
+  `id` int(11) NOT NULL,
+  `userId` int(11) DEFAULT NULL,
+  `roomId` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `photo`
+--
+
+CREATE TABLE `photo` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` varchar(1000) DEFAULT NULL,
+  `albumId` int(11) NOT NULL,
+  `url` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `photo`
+--
+
+INSERT INTO `photo` (`id`, `name`, `description`, `albumId`, `url`) VALUES
+(1, 'pexels-brandan-saviour-2741701\r\n', 'test image', 1, 'img/pexels-brandan-saviour-2741701.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `room`
+--
+
+CREATE TABLE `room` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userInterest`
+--
+
+CREATE TABLE `userInterest` (
+  `id` int(11) NOT NULL,
+  `userId` int(11) DEFAULT NULL,
+  `interestId` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `userInterest`
+--
+
+INSERT INTO `userInterest` (`id`, `userId`, `interestId`) VALUES
+(1, 1, 2),
+(2, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userLookingForId`
+--
+
+CREATE TABLE `userLookingForId` (
+  `id` int(11) NOT NULL,
+  `userId` int(11) DEFAULT NULL,
+  `gender` enum('male','female','other') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
 
 CREATE TABLE `users` (
-`userName` varchar(255) NOT NULL,
-`email` varchar(255) NOT NULL,
-`id` int(11) NOT NULL,
-`role` varchar(255) NOT NULL,
-`birthDate` datetime NOT NULL,
-`genderId` int(11) NOT NULL,
-`bio` varchar(500) NULL,
-`password` varchar(255) NOT NULL,
-`userInterestsId` int(11) NULL,
-`userLookingForId` int(11) NULL,
-PRIMARY KEY (`userName`) ,
-INDEX ()
-);
-CREATE TABLE `albums` (
-`id` int(11) NOT NULL,
-`name` varchar(255) NULL,
-`description` varchar(255) NULL,
-`category` varchar(255) NULL,
-`ownerId` int(11) NULL,
-PRIMARY KEY (`id`) 
-);
-CREATE TABLE `photo` (
-`id` int(11) NOT NULL,
-`name` varchar(255) NOT NULL,
-`description` varchar(1000) NULL,
-`albumId` int(11) NOT NULL,
-`fileId` int(11) NULL,
-`url` varchar(255) NULL,
-PRIMARY KEY (`id`) 
-);
-CREATE TABLE `message` (
-`id` int(11) NOT NULL,
-`roomId` int(11) NULL,
-`userId` int(11) NULL,
-`message` varchar(1000) NULL,
-PRIMARY KEY (`id`) 
-);
-CREATE TABLE `room` (
-`id` int(11) NOT NULL,
-`name` varchar(255) NULL,
-`roomType` int(11) NULL,
-PRIMARY KEY (`id`) 
-);
-CREATE TABLE `participants` (
-`id` int(11) NOT NULL,
-`userId` int(11) NULL,
-`roomId` int(11) NULL,
-PRIMARY KEY (`id`) 
-);
-CREATE TABLE `roomType` (
-`id` int(11) NOT NULL,
-`name` varchar(255) NULL,
-PRIMARY KEY (`id`) 
-);
-CREATE TABLE `genders` (
-`id` int(11) NOT NULL,
-`name` varchar(255) NULL,
-PRIMARY KEY (`id`) 
-);
-CREATE TABLE `matches` (
-`uId1` int(11) NOT NULL,
-`uId2` int(11) NOT NULL,
-`Id` int(11) NOT NULL,
-`isActive` bit(1) NULL,
-PRIMARY KEY (`Id`) 
-);
-CREATE TABLE `interests` (
-`id` int(11) NOT NULL,
-`name` varchar(255) NULL,
-`description` varchar(500) NULL,
-`type` varchar(255) NULL,
-PRIMARY KEY (`id`) 
-);
-CREATE TABLE `userInterest` (
-`id` int(11) NOT NULL,
-`userId` int(11) NULL,
-`interestId` int(11) NULL,
-PRIMARY KEY (`id`) 
-);
-CREATE TABLE `userLookingForId` (
-`id` int(11) NOT NULL,
-`userId` int(11) NULL,
-`genderId` int(11) NULL,
-PRIMARY KEY (`id`) 
-);
+  `userName` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `id` int(11) NOT NULL,
+  `role` enum('admin','user') DEFAULT 'user',
+  `birthDate` datetime DEFAULT NULL,
+  `genderId` int(11) DEFAULT NULL,
+  `bio` varchar(500) DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `userInterestsId` int(11) DEFAULT NULL,
+  `userLookingForId` int(11) DEFAULT NULL,
+  `token` varchar(1000) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-ALTER TABLE `room` ADD CONSTRAINT `fk_room` FOREIGN KEY (`roomType`) REFERENCES `roomType` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-ALTER TABLE `participants` ADD CONSTRAINT `fk_participants` FOREIGN KEY (`roomId`) REFERENCES `room` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-ALTER TABLE `participants` ADD CONSTRAINT `fk_participants_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-ALTER TABLE `message` ADD CONSTRAINT `fk_message` FOREIGN KEY (`roomId`) REFERENCES `room` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-ALTER TABLE `message` ADD CONSTRAINT `fk_message_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-ALTER TABLE `photo` ADD CONSTRAINT `fk_photo` FOREIGN KEY (`albumId`) REFERENCES `albums` (`id`);
-ALTER TABLE `albums` ADD CONSTRAINT `fk_albums_1` FOREIGN KEY (`ownerId`) REFERENCES `users` (`id`);
-ALTER TABLE `users` ADD CONSTRAINT `fk_users_1` FOREIGN KEY (`genderId`) REFERENCES `genders` (`id`);
-ALTER TABLE `matches` ADD CONSTRAINT `fk_matches` FOREIGN KEY (`uId1`) REFERENCES `users` (`id`);
-ALTER TABLE `matches` ADD CONSTRAINT `fk_matches_1` FOREIGN KEY (`uId2`) REFERENCES `users` (`id`);
-ALTER TABLE `userInterest` ADD CONSTRAINT `fk_userInterest` FOREIGN KEY (`userId`) REFERENCES `users` (`id`);
-ALTER TABLE `userInterest` ADD CONSTRAINT `fk_userInterest_1` FOREIGN KEY (`interestId`) REFERENCES `interests` (`id`);
-ALTER TABLE `userLookingForId` ADD CONSTRAINT `fk_userLookingForId` FOREIGN KEY (`genderId`) REFERENCES `genders` (`id`);
-ALTER TABLE `userLookingForId` ADD CONSTRAINT `fk_userLookingForId_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`);
+--
+-- Dumping data for table `users`
+--
 
+INSERT INTO `users` (`userName`, `email`, `id`, `role`, `birthDate`, `genderId`, `bio`, `password`, `userInterestsId`, `userLookingForId`, `token`) VALUES
+('test2093', 'test@gmail.com', 1, 'user', '1994-11-13 00:00:00', 1, 'fiawdifhu aiuwhfoia aweihufwoi aiwuh iaweiofh iahwefiuewf iiwhefi u', 'muffin', 2, 2, NULL),
+('signuptest', 'signup@signup.com', 2, 'user', NULL, NULL, NULL, 'Min123456', NULL, NULL, NULL),
+('tokentest', 'signup', 3, 'user', NULL, NULL, NULL, 'Min123456', NULL, NULL, NULL),
+('tokentest2', 'signup@token.com', 4, 'user', NULL, NULL, NULL, 'Min123456', NULL, NULL, NULL),
+('tokentest3', 'signup@token3.com', 5, 'user', NULL, NULL, NULL, 'Min123456', NULL, NULL, 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJmYXN0ZGF0aW5nLm9yZyIsImF1ZCI6ImZhc3RkYXRpbmcuY2EiLCJpYXQiOjUsIm5iZiI6InRva2VudGVzdDMiLCJleGQiOiIyMDIxLTEyLTExIn0.BpZRis7toDKdXPlkMv1LBk1tt0GHwu65ANyp5sA7XkYgnZqSb-c4OJwUaAAUPsNsXm8QNzIUx5s8xbD5YA7_-3DgRBGR4eX3MCBYsd7OsXBK1CRN_bbtczE9GHAK5Hi8l2xRQW1jkgZJtrsLYJJ4kp3elyPEsjvUnpzXmlzY4tE'),
+('testtoken 6', 'testtoken6@test.com', 6, 'user', NULL, NULL, NULL, 'Min123456', NULL, NULL, 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJmYXN0ZGF0aW5nLm9yZyIsImF1ZCI6ImZhc3RkYXRpbmcuY2EiLCJpYXQiOjYsIm5iZiI6InRlc3R0b2tlbiA2IiwiZXhkIjoiMjAyMS0xMi0xMSJ9.CaNzz950_3BHdl9SQes4I79O06HIrrivn-i_ET8paUA--WZQHucJMnlGN0DXZQABZea05skMKBCnhnvTXjor84Y7uA6Tuu_0J642E-4P0b0GTh8mJJAjA0p39Upcd3oMSv37oGNjtS3m9sZPHGvyndVnku9gJjkQbPEcEkCQt7M'),
+('testtoken7', 'testtoken7@test.com', 7, 'user', NULL, NULL, NULL, 'Min123456', NULL, NULL, 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJmYXN0ZGF0aW5nLm9yZyIsImF1ZCI6ImZhc3RkYXRpbmcuY2EiLCJpYXQiOjcsIm5iZiI6InRlc3R0b2tlbjciLCJleGQiOiIyMDIxLTEyLTExIn0.GiJlib1pc5IKwy1yLEJQOQPmrcASBs9_Bg-e_clgbN5yOoPXpN8pLjjch40LTLXo0a7pweAL7F-8RpjyXROuc-RhhJWpXQ9F__w5AyXehggaWdr5K3uzppzQ1rfwQx-k-WO7FoTuAhdEHIG4NixeK4ZQ_f0C6HGy1NThoMR5iW4'),
+('token10', 'token10@token10.com', 8, 'user', NULL, NULL, NULL, 'token10', NULL, NULL, 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJmYXN0ZGF0aW5nLm9yZyIsImF1ZCI6ImZhc3RkYXRpbmcuY2EiLCJpYXQiOjgsIm5iZiI6InRva2VuMTAiLCJleGQiOiIyMDIxLTEyLTExIn0.EcJEXuP8VaDXH9F4DAg3wfgi9-XmW53OT7nT2sMQV1fMcV5cvCWNrV7Tcx-x8Y3Tdenub9TtuhPxaRVBEgH1tuphkm-g0PHgObqteULaaWR1KL1BKFbJ9gq-hISX7gLDMZnjRGy4ID1t4GbF3Y0rINzgFf710Ma_Oehs1pz6vLg'),
+('token12', 'token12@token12.com', 9, 'user', NULL, NULL, NULL, 'token12@A', NULL, NULL, 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJmYXN0ZGF0aW5nLm9yZyIsImF1ZCI6ImZhc3RkYXRpbmcuY2EiLCJpYXQiOjksIm5iZiI6InRva2VuMTIiLCJleGQiOiIyMDIxLTEyLTExIn0.XflyD9FZ5DBBJ0bBwVO0fxv1daDeEbrFSq-w4FAuHwwBndgSfx10KX3hlNUJdDQSqDhlFppyf_4HbRA8rG9z9qR0zU7F3vSbrSFSjkpSEj4qK9FyOO9Q86gBaT24JZRlNUM4UY3Lt8InzxU8xJWEuy9ZIS2tv1mIhjUYpsxgEXg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `albums`
+--
+ALTER TABLE `albums`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fkOwenerId` (`ownerId`);
+
+--
+-- Indexes for table `interests`
+--
+ALTER TABLE `interests`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `matches`
+--
+ALTER TABLE `matches`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indexes for table `message`
+--
+ALTER TABLE `message`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fkMessageUId` (`userId`),
+  ADD KEY `fkMessageRoom` (`roomId`);
+
+--
+-- Indexes for table `participants`
+--
+ALTER TABLE `participants`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fkParticipantsUId` (`userId`),
+  ADD KEY `fkParticipantsRoom` (`roomId`);
+
+--
+-- Indexes for table `photo`
+--
+ALTER TABLE `photo`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fkAlbumPhoto` (`albumId`);
+
+--
+-- Indexes for table `room`
+--
+ALTER TABLE `room`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `userInterest`
+--
+ALTER TABLE `userInterest`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_userInterest_1` (`interestId`),
+  ADD KEY `fkUserInterestUsers` (`userId`);
+
+--
+-- Indexes for table `userLookingForId`
+--
+ALTER TABLE `userLookingForId`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fkUserLookingFor` (`userId`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `albums`
+--
+ALTER TABLE `albums`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `matches`
+--
+ALTER TABLE `matches`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `message`
+--
+ALTER TABLE `message`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `participants`
+--
+ALTER TABLE `participants`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `photo`
+--
+ALTER TABLE `photo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `room`
+--
+ALTER TABLE `room`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `userInterest`
+--
+ALTER TABLE `userInterest`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `userLookingForId`
+--
+ALTER TABLE `userLookingForId`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `albums`
+--
+ALTER TABLE `albums`
+  ADD CONSTRAINT `fkOwenerId` FOREIGN KEY (`ownerId`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `matches`
+--
+ALTER TABLE `matches`
+  ADD CONSTRAINT `fkUId1` FOREIGN KEY (`Id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `fkUId2` FOREIGN KEY (`Id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `message`
+--
+ALTER TABLE `message`
+  ADD CONSTRAINT `fkMessageRoom` FOREIGN KEY (`roomId`) REFERENCES `room` (`id`),
+  ADD CONSTRAINT `fkMessageUId` FOREIGN KEY (`userId`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `participants`
+--
+ALTER TABLE `participants`
+  ADD CONSTRAINT `fkParticipantsRoom` FOREIGN KEY (`roomId`) REFERENCES `room` (`id`),
+  ADD CONSTRAINT `fkParticipantsUId` FOREIGN KEY (`userId`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `photo`
+--
+ALTER TABLE `photo`
+  ADD CONSTRAINT `fkAlbumPhoto` FOREIGN KEY (`albumId`) REFERENCES `albums` (`id`);
+
+--
+-- Constraints for table `userInterest`
+--
+ALTER TABLE `userInterest`
+  ADD CONSTRAINT `fkUserInterestUsers` FOREIGN KEY (`userId`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `fk_userInterest_1` FOREIGN KEY (`interestId`) REFERENCES `interests` (`id`);
+
+--
+-- Constraints for table `userLookingForId`
+--
+ALTER TABLE `userLookingForId`
+  ADD CONSTRAINT `fkUserLookingFor` FOREIGN KEY (`userId`) REFERENCES `users` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
