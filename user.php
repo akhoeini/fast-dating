c959ca0a7f257fdbfa17af0ad19731ebbc7db6b0
kpjac <6174409@johnabbottcollege.net>
2021-11-12 13:43:17 -0500

    basic edit-profile handling logic

--- user.php
+++ user.php
@@ -155,7 +155,7 @@ $app->get('/profile/{uId:[0-9]+}', function ($request, $response, $args) {
         throw new \Slim\Exception\NotFoundException($request, $response);
     } else {
         // Will display 'edit profile' button if user is viewing own profile
-        $isOwnProfile = ($args['uId'] == $_SESSION['userId']);
+        $isOwnProfile = ($args['uId'] == $_SESSION['user']['id']);
         return $this->view->render($response, 'profile.html.twig', ['u' => $userInfo, 'pics' => $userPhotos, 'interests' => $userInterests, 'ownProfile' => $isOwnProfile]);
     }
 });
@@ -167,15 +167,13 @@ $app->get('/edit-profile', function ($request, $response, $args) {
         $response = $response->withStatus(302);
         return $response->withHeader('Location', '/login');
     };
-    $userInfo = DB::queryFirstRow("SELECT * FROM users WHERE id=%i", $args['uId']);
-    $userPhotos = DB::query("SELECT photo.name, photo.description, photo.url FROM albums, photo WHERE albums.ownerId=%i AND albums.id=photo.albumId", $args['uId']);
-    $userInterests = DB::queryFirstColumn("SELECT interests.name FROM userInterest, interests WHERE userInterest.userId=%i AND userInterest.interestId=interests.id", $args['uId']);
+    $userInfo = DB::queryFirstRow("SELECT * FROM users WHERE id=%i", $userId);
+    $userPhotos = DB::query("SELECT photo.name, photo.description, photo.url FROM albums, photo WHERE albums.ownerId=%i AND albums.id=photo.albumId", $userId);
+    $userInterests = DB::queryFirstColumn("SELECT interests.name FROM userInterest, interests WHERE userInterest.userId=%i AND userInterest.interestId=interests.id", $userId);
     if (!$userInfo) {
         throw new \Slim\Exception\NotFoundException($request, $response);
     } else {
-        // Will display 'edit profile' button if user is viewing own profile
-        $isOwnProfile = ($args['uId'] == $_SESSION['userId']);
-        return $this->view->render($response, 'profile.html.twig', ['u' => $userInfo, 'pics' => $userPhotos, 'interests' => $userInterests, 'ownProfile' => $isOwnProfile]);
+        return $this->view->render($response, 'editprofile.html.twig', ['u' => $userInfo, 'pics' => $userPhotos, 'interests' => $userInterests]);
     }
 });
 
