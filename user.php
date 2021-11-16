28bd5dd9b1123eaafbc1513d01c64113b3fc939b
kpjac <6174409@johnabbottcollege.net>
2021-11-16 00:10:46 -0500

    patch

--- user.php
+++ user.php
@@ -52,24 +52,7 @@ $app->patch('/edit-profile', function ($request, $response) use ($log) {
     };
     $json = $request->getBody();
     $profile = json_decode($json, TRUE);
-    /*
-    $firstName = $request->getParam('firstName');
-    $location = $request->getParam('location');
-    $username = $request->getParam('username');
-    $email = $request->getParam('email');
-    $gender = $request->getParam('gender');
-    $genderLFm = $request->getParam('genderLFm');
-    $genderLFf = $request->getParam('genderLFf');
-    $genderLF = 3;
-    if (!$genderLFm) {
-        $genderLF = 2;
-    }
-    if (!$genderLFf) {
-        $genderLF = 1;
-    }
-    $bio = $request->getParam('bio');
-    */
-
+ 
     // success
     $valuesList = ['firstName' => $profile['firstName'], 'location' => $profile['location'], 'username' => $profile['username'], 'email' => $profile['email'], 'gender' => $profile['gender'], 'userLookingForId' => $profile['genderLF'], 'bio' => $profile['bio']];
     DB::update('users', $valuesList, "id=%i", $userId);
